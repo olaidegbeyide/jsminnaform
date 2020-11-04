232201c28295c04c8eeaad3f27437a66f97a4dca
olaidegbeyide <toungbeyide9@gmail.com>
2020-11-04 18:51:14 +0100

    buggggg

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
-const firstname = document.getElementById('fname').value
-const lastname = document.getElementById('lname').value
-const email = document.getElementById('email').value
-const password = document.getElementById('password').value
+const firstname = document.getElementById('fname')
+const lastname = document.getElementById('lname')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
 const confirmpass = document.getElementById('cpassword')
 const form = document.getElementById('myform')
+const button = document.getElementById('btn')
 
 
 form.addEventListener('submit', (e) => {
       e.preventDefault()
-    if (firstname === '' || firstname === null) {
+   
+    checkInput()
+})
+
+
+function checkInput () {
+    if (firstname.value === '' || firstname.value === null) {
         document.getElementById('fname-error').innerText = 'Please enter your First name'
     } else {
         document.getElementById('fname-error').innerText = ''
     }
-    if (lastname == '' || lastname == null) {
+    if (lastname.value == '' || lastname.value == null) {
         document.getElementById('lname-error').innerText = 'Please enter your Last name'
+    } else {
+        document.getElementById('lname-error').innerText = ''
     }
-    if (email == ''  || email == null ) {
+    if (email.value == ''  || email.value == null ) {
         document.getElementById('mail-error').innerText = 'Please enter a valid email address'
+    } else {
+        document.getElementById('mail-error').innerText = ''
     }
-    if (password < 8  ) {
+    if (password.value < 8  ) {
         document.getElementById('password-error').innerText = 'Password must be up to eight characters'
     }
-    else if (password.includes(Number) || password.value.includes(Symbol)) {
-        document.getElementById('password-error').innerText = 'Password is strong'
+    if (confirmpass.value !== password.value) {
+        document.getElementById('cpassword-error').innerText = 'Incorrect Password'
+    } else {
+        document.getElementById('cpassword-error').innerText = ''
     }
-    
-})
+}
+
+function checkbutton() {
+    if (!checkInput()) {
+        button.setAttribute('disabled','disabled')
+    } else {
+        button.removeAttribute('disabled')
+    }
+}
+
+ checkbutton()
